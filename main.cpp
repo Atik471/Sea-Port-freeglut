ef8c975dfb591dbd1c285cda891343a4c66d9d12
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-01-13 23:37:34 +0600

    added container

--- main.cpp
+++ main.cpp
@@ -118,16 +118,47 @@ void drawWave(float startX, float startY, float length, string color)
 
 void drawBgBuildings()
 {
-	/*glColor3f(0.443, 0.749, 0.824);
+	glColor3f(0.443, 0.749, 0.824);
     glBegin(GL_POLYGON);
-    glVertex2f(0, 230);
-    glVertex2f(1400, 230);
-    glVertex2f(1300, 260);
-	glVertex2f(500, 280);
-    glVertex2f(0, 260);
-    //glVertex2f(40, 180);
-    //glVertex2f(50, 200);
-    glEnd();*/
+	/*glVertex2f(0, 230);
+    glVertex2f(0, 380);
+    glVertex2f(30, 380);
+    glVertex2f(30, 350);
+	glVertex2f(200, 350);
+	glVertex2f(200, 430);
+	glVertex2f(250, 430);
+	glVertex2f(250, 330);
+	glVertex2f(320, 330);
+	glVertex2f(320, 230);*/
+    glEnd();
+}
+
+void drawContainer(float startX, float startY, string color)
+{
+	if(color == "p") glColor3f(0.416, 0.204, 0.533);
+	else if(color == "y") glColor3f(0.902, 0.866, 0.149);
+	else if(color == "b") glColor3f(0.145, 0.694, 0.855);
+	else if(color == "r") glColor3f(0.859, 0.196, 0.224);
+	
+	glBegin(GL_QUADS);
+	glVertex2f(startX, startY);
+	glVertex2f(startX + 100, startY);
+	glVertex2f(startX + 100, startY + 50);
+	glVertex2f(startX, startY + 50);
+	glEnd();
+	
+	if(color == "p") glColor3f(0.711, 0.176, 1);
+	else if(color == "y") glColor3f(0.737, 0.702, 0);
+	else if(color == "b") glColor3f(0.380, 0.796, 1);
+	else if(color == "r") glColor3f(1, 0.471, 0.494);
+	glLineWidth(2);
+	glBegin(GL_LINES);
+	for(int i=10; i<100; i+=10)
+	{
+		glVertex2f(startX + i, startY + 5);
+		glVertex2f(startX + i, startY + 45);
+	}
+	glEnd();
 }
 
 void display()
@@ -136,15 +167,39 @@ void display()
 	drawSky();
 	drawWater();
 	drawLand();
+	
 	drawWave(25, 60, 100, "dark");
 	drawWave(185, 95, 200, "light");
 	drawWave(1100, 25, 200, "dark");
 	drawWave(225, 30, 100, "dark");
-	//drawWave(725, 95, 300, "dark");
 	drawWave(900, 65, 250, "light");
 	drawWave(1200, 105, 150, "dark");
 	drawWave(400, 35, 350, "dark");
+	
 	drawBgBuildings();
+	
+	
+	drawContainer(300, 230, "p");
+	drawContainer(400, 230, "y");
+	drawContainer(500, 230, "r");
+	drawContainer(600, 230, "b");
+	drawContainer(700, 230, "y");
+	drawContainer(800, 230, "r");
+	drawContainer(900, 230, "p");
+	drawContainer(1000, 230, "b");
+	drawContainer(1100, 230, "y");
+	drawContainer(1200, 230, "r");
+	drawContainer(1300, 230, "b");
+	drawContainer(320, 280, "r");
+	drawContainer(430, 280, "b");
+	drawContainer(580, 280, "y");
+	drawContainer(730, 280, "p");
+	drawContainer(850, 280, "y");
+	drawContainer(1050, 280, "b");
+	drawContainer(1150, 280, "y");
+	drawContainer(1300, 280, "p");
+	drawContainer(1100, 330, "r");
+	
     glFlush();
 }
 
